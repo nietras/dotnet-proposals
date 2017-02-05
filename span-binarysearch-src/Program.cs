a4d5d391264920da6a44a35773b82e118ff9f333
ntr <ntr@ihfood.dk>
2017-02-05 11:32:28 +0100

    span-binarysearch: Add compound example and Span<T> overloads.

--- span-binarysearch-src/Program.cs
+++ span-binarysearch-src/Program.cs
@@ -13,9 +13,6 @@ namespace System
 
     public static class ReadOnlySpanExtensions
     {
-        //public static int BinarySearch<T>(this ReadOnlySpan<T> span, T value) 
-        //{ throw null; }
-
         public static int BinarySearch<T, TComparable>(this ReadOnlySpan<T> span, TComparable comparable) 
             where TComparable : IComparable<T> 
         { throw null; }
@@ -23,9 +20,19 @@ namespace System
         public static int BinarySearch<T, TComparer>(this ReadOnlySpan<T> span, T value, TComparer comparer) 
             where TComparer : IComparer<T>
         { throw null; }
+
+        // NOTE: Due to the less-than-ideal generic type inference in the face of implicit conversions,
+        //       we need the overloads taking Span<T>.
+        public static int BinarySearch<T, TComparable>(this Span<T> span, TComparable comparable) 
+            where TComparable : IComparable<T> 
+        { return BinarySearch((ReadOnlySpan<T>)span, comparable); }
+
+        public static int BinarySearch<T, TComparer>(this Span<T> span, T value, TComparer comparer) 
+            where TComparer : IComparer<T>
+        { return BinarySearch((ReadOnlySpan<T>)span, value, comparer); }
     }
 
-    public static class Usage
+    public static class UsageForInt
     {
         struct InlineableComparer : IComparer<int>
         {
@@ -56,7 +63,7 @@ namespace System
             }
         }
 
-        public static void BinarySearch()
+        public static void SpanBinarySearch()
         {
             var span = new Span<int>();
 
@@ -75,13 +82,116 @@ namespace System
             var comparable = new InlineableComparable(valueToFind);
             index = span.BinarySearch(comparable);
         }
+
+        public static void ReadOnlySpanBinarySearch()
+        {
+            ReadOnlySpan<int> span = new Span<int>();
+
+            //span.Sort();
+
+            int valueToFind = 42;
+
+            // Direct value
+            var index = span.BinarySearch(valueToFind);
+
+            // Inlineable struct comparer
+            var comparer = new InlineableComparer();
+            index = span.BinarySearch(valueToFind, comparer);
+
+            // Inlineable struct comparable
+            var comparable = new InlineableComparable(valueToFind);
+            index = span.BinarySearch(comparable);
+        }
+    }
+
+    public static class UsageForCompound
+    {
+#pragma warning disable 0649 // Unused/unassigned warning, not important here
+        struct Compound
+        {
+            public float FeatureValue;
+            public int FeatureIndex;
+            public object Payload;
+        }
+#pragma warning restore 0649
+
+        struct InlineableFeatureValueComparer : IComparer<Compound>
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int Compare(Compound a, Compound b)
+            {
+                if (a.FeatureValue == b.FeatureValue) { return 0; }
+                if (a.FeatureValue < b.FeatureValue) { return -1; }
+                return 1;
+            }
+        }
+
+        struct InlineableFeatureComparable : IComparable<Compound>
+        {
+            readonly float m_featureValue;
+            
+            public InlineableFeatureComparable(float featureValue)
+            {
+                m_featureValue = featureValue;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int CompareTo(Compound other)
+            {
+                if (m_featureValue == other.FeatureIndex) { return 0; }
+                if (m_featureValue < other.FeatureIndex) { return -1; }
+                return 1;
+            }
+        }
+
+        public static void SpanBinarySearch()
+        {
+            var span = new Span<Compound>();
+
+            //span.Sort(new InlineableFeatureValueComparer());
+
+            float featureValueToFind = 1.234f;
+
+            // Inlineable struct comparer
+            var comparer = new InlineableFeatureValueComparer();
+            // Less than ideal for compound, which is reason for comparable overload
+            var compound = new Compound(){ FeatureValue = featureValueToFind };
+            var index = span.BinarySearch(compound, comparer);
+
+            // Inlineable struct comparable (easier to use for compounded type)
+            var comparable = new InlineableFeatureComparable(featureValueToFind);
+            index = span.BinarySearch(comparable);
+        }
+
+        public static void ReadOnlySpanBinarySearch()
+        {
+            ReadOnlySpan<Compound> span = new Span<Compound>();
+
+            //span.Sort(new InlineableFeatureValueComparer());
+
+            float featureValueToFind = 1.234f;
+
+            // Inlineable struct comparer
+            var comparer = new InlineableFeatureValueComparer();
+            // Less than ideal for compound, which is reason for comparable overload
+            var compound = new Compound(){ FeatureValue = featureValueToFind };
+            var index = span.BinarySearch(compound, comparer);
+
+            // Inlineable struct comparable (easier to use for compounded type)
+            var comparable = new InlineableFeatureComparable(featureValueToFind);
+            index = span.BinarySearch(comparable);
+        }
     }
 
     public static class Program
     {
         public static void Main()
         {
-            Usage.BinarySearch();
+            UsageForInt.SpanBinarySearch();
+            UsageForInt.ReadOnlySpanBinarySearch();
+
+            UsageForCompound.SpanBinarySearch();
+            UsageForCompound.ReadOnlySpanBinarySearch();
         }
     }
 }
